14467ec8ff467db420df8c60ac8b1fb50ba93522
Bailey Atwood <flyer95.ba@gmail.com>
2020-10-25 00:05:11 -0400

    Implimented More Advanced Patroling
    No longer travels to Waypoints in a specific order but rather based on proximity.

--- VGDC Patrolling/Assets/Script/Person.cs
+++ VGDC Patrolling/Assets/Script/Person.cs
@@ -2,155 +2,155 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
-using UnityEngine.UI;
 
-public class Person : MonoBehaviour
+namespace Assets.Code
 {
-    [Header("Person AI")]
-    // Whether the Person waits on each Point
-    [SerializeField]
-    bool patrolWaiting = true;
-
-    // Time to wait at each Point
-    [SerializeField]
-    float waitTime = 3f;
-
-    // Probability of switching direction
-    [SerializeField]
-    float switchProb = 0.2f;
-
-    // List of Points to vist
-    [SerializeField]
-    List<Point> patrolPoints = null;
-
-    // Private variables for behaviour
-    NavMeshAgent agent;
-    PersonSight personSight;
-    int currentPatrolIndex;
-    bool traveling;
-    bool waiting;
-    bool patrolForward;
-    float waitTimer;
-
-    [Header("Detection")]
-    public DetectionBar detectionBar;
-    [HideInInspector]
-    public Transform lastLocation = null;
-    public float rotationSpeed = 10f;
-
-    private void Start()
+    public class Person : MonoBehaviour
     {
-        agent = this.GetComponent<NavMeshAgent>();
-        personSight = this.GetComponent<PersonSight>();
+        [Header("Person AI")]
+        // Whether the Person waits on each Point
+        [SerializeField]
+        bool patrolWaiting = true;
 
-        if (patrolPoints != null && patrolPoints.Count >= 2)
-        {
-            currentPatrolIndex = 0;
-            SetDestination();
-        } else
-        {
-            Debug.LogError("Need more than 2 Points on " + gameObject.name);
-        }
-    }
+        // Time to wait at each Point
+        [SerializeField]
+        float waitTime = 3f;
 
-    private void Update()
-    {
-        if (!personSight.ghostSpotted)
+        // Probability of switching direction
+        [SerializeField]
+        float switchProb = 0.2f;
+
+        // List of Points to vist
+        [SerializeField]
+        List<Point> patrolPoints = null;
+
+        // Private variables for behaviour
+        NavMeshAgent agent;
+        ConnectedWaypoint currentWaypoint;
+        ConnectedWaypoint previousWaypoint;
+
+        PersonSight personSight;
+        bool traveling;
+        bool waiting;
+        float waitTimer;
+        int pointsVisited;
+
+        [Header("Detection")]
+        public DetectionBar detectionBar;
+        [HideInInspector]
+        public Transform lastLocation = null;
+        public float rotationSpeed = 10f;
+
+        private void Start()
         {
-            agent.isStopped = false;
-            lastLocation = null;
-            // Check if close to destination
-            if (traveling && agent.remainingDistance <= 1.0f)
+            agent = this.GetComponent<NavMeshAgent>();
+            personSight = this.GetComponent<PersonSight>();
+
+            if(currentWaypoint == null)
             {
-                traveling = false;
+                // Set it to random
+                // Grab all Points in Scene
+                GameObject[] allWaypoints = GameObject.FindGameObjectsWithTag("Point");
 
-                // Wait if waiting
-                if (patrolWaiting)
+                if(allWaypoints.Length > 0)
                 {
-                    waiting = true;
-                    waitTimer = 0f;
+                    while (currentWaypoint == null)
+                    {
+                        int random = UnityEngine.Random.Range(0, allWaypoints.Length);
+                        ConnectedWaypoint startingWaypoint = allWaypoints[random].GetComponent<ConnectedWaypoint>();
+
+                        // If found a Point
+                        if (startingWaypoint != null)
+                        {
+                            currentWaypoint = startingWaypoint;
+                        }
+                    }
                 }
                 else
                 {
-                    ChangePatrolPoint();
-                    SetDestination();
+                    Debug.LogError("Failed to find any Points");
                 }
-
             }
 
-            // Instead, if we're waiting
-            if (waiting)
+            SetDestination();
+        }
+
+        private void Update()
+        {
+            if (!personSight.ghostSpotted)
             {
-                waitTimer += Time.deltaTime;
-                if (waitTimer >= waitTime)
-                {
-                    waiting = false;
+                agent.isStopped = false;
+                lastLocation = null;
 
-                    ChangePatrolPoint();
-                    SetDestination();
+                // Check if close to destination
+                if (traveling && agent.remainingDistance <= 1.0f)
+                {
+                    traveling = false;
+                    pointsVisited++;
+
+                    // Wait if waiting
+                    if (patrolWaiting)
+                    {
+                        waiting = true;
+                        waitTimer = 0f;
+                    }
+                    else
+                    {
+                        SetDestination();
+                    }
+
+                }
+
+                // Instead, if we're waiting
+                if (waiting)
+                {
+                    waitTimer += Time.deltaTime;
+                    if (waitTimer >= waitTime)
+                    {
+                        waiting = false;
+
+                        SetDestination();
+                    }
                 }
             }
-        } else
-        {
-            GhostSeen();
+            else
+            {
+                GhostSeen();
+            }
         }
-    }
 
-    public void GhostSeen()
-    {
-        //Toggle movement
-        agent.isStopped = true;
-
-        // Restart and start Timer
-        //detectionBar.alert = detectionBar.maxAlert;
-        detectionBar.TimerOn = true;
-
-        if(lastLocation != null)
+        public void GhostSeen()
         {
-            // Look towards lastLocation
-            lastLocation = personSight.targetLastLocation.transform;
-            Vector3 lookPos = lastLocation.position - transform.position;
-            lookPos.y = 0;
-            var rotation = Quaternion.LookRotation(lookPos);
-            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * 2);
+            //Toggle movement
+            agent.isStopped = true;
+
+            // Restart and start Timer
+            //detectionBar.alert = detectionBar.maxAlert;
+            detectionBar.TimerOn = true;
+
+            if (lastLocation != null)
+            {
+                // Look towards lastLocation
+                lastLocation = personSight.targetLastLocation.transform;
+                Vector3 lookPos = lastLocation.position - transform.position;
+                lookPos.y = 0;
+                var rotation = Quaternion.LookRotation(lookPos);
+                transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * 2);
+            }
         }
-    }
 
-    private void SetDestination()
-    {
-        if (patrolPoints != null)
+        private void SetDestination()
         {
-            // Get Vector of current Patrol Point from List
-            Vector3 targetVector = patrolPoints[currentPatrolIndex].transform.position;
-            // Set destination to Vector and travel to the Point
+            if (pointsVisited > 0)
+            {
+                ConnectedWaypoint nextWaypoint = currentWaypoint.NextWaypoint(previousWaypoint);
+                previousWaypoint = currentWaypoint;
+                currentWaypoint = nextWaypoint;
+            }
+
+            Vector3 targetVector = currentWaypoint.transform.position;
             agent.SetDestination(targetVector);
             traveling = true;
         }
     }
-
-    // Selects a new Patrol Point in the list with a probability for moving forward or backwards
-    private void ChangePatrolPoint()
-    {
-        // Random number between 0-1 & if less than Probability
-        if (UnityEngine.Random.Range(0f, 01) <= switchProb)
-        {
-            // Patrol forward or backwards
-            patrolForward = !patrolForward;
-        }
-
-        // Check if "overlapping" list (Can't go back if you're at the top of the list)
-        if (patrolForward)
-        {
-
-            currentPatrolIndex = (currentPatrolIndex + 1) % patrolPoints.Count;
-        }
-        else
-        {
-            if(--currentPatrolIndex < 0)
-            {
-                currentPatrolIndex = patrolPoints.Count - 1;
-            }
-        }
-    }
-
 }

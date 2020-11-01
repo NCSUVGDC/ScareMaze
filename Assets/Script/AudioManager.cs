0970d315d07ed2891f852bb8a559950da2949647
Will <willcarpenter3@yahoo.com>
2020-11-01 18:30:36 -0500

    Added audio stuff

---
+++ Assets/Script/AudioManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+
+    public AudioSource music;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        music.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
